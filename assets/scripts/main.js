6827c89c736cd84004cac12eda119ea48a70ccc0
plake <plake492@gmail.com>
2019-06-29 22:51:44 -0400

    finished%95 of js. Added custom jont

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,62 +1,54 @@
+
 //ATTACK CHARACTER & OPPONANT 
 let attackCharacter = []
-
 let opponentCharacter = []
 
-// CHARACTERS
 let characters =[
     aragorn = {
         name: "Aragorn",
-        health: 160,
-        attack: 25,
-        counterAttack: 15,
-
-        // increaseAttack()  {
-        //     this.attack = this.attack * 2;
-        // }
+        health: 120,
+        attack: 8,
+        counterAttack: 18,
     },
     sauron = {
         name: "Sauron",
-        health: 200,
-        attack: 15,
-        counterAttack: 20,
-
-        // increaseAttack()  {
-        //     this.attack = this.attack * 2;
-        // }
+        health: 180,
+        attack: 6,
+        counterAttack: 25,
     },
     gandalf = {
         name: "Gandalf",
-        health: 140,
-        attack: 20,
-        counterAttack: 40,
-
-        // increaseAttack()  {
-        //     this.attack = this.attack * 2;
-        // }
+        health: 100,
+        attack: 10,
+        counterAttack: 25,
     },
     witchKing = {
         name:"Witch King",
-        health: 180,
-        attack: 30,
-        counterAttack: 10,
-
-        // increaseAttack()  {
-        //     this.attack = this.attack * 2;
-        // }
+        health: 150,
+        attack: 12,
+        counterAttack: 15,
     },
 ]
+
+
     console.log(characters)
 
         function goThereA() {
             if (attackCharacter.length === 0 && opponentCharacter.length === 0) {
                 attackCharacter.push(aragorn)
                 $("#aragorn").appendTo("#playerCharacter");
+                $("#yourCharacterText").text("Your Character");
+                $("#ar").prop("onclick", null).off("click");
+                $("#ar").attr("id", "attacker");
+                $('#characterOrOpponent').text("Choose your opponent")
             } else if (attackCharacter.length === 1 && opponentCharacter.length === 0) { 
                 opponentCharacter.push(aragorn)
                 $("#aragorn").appendTo("#opponent");
+                $("#ar").attr("id", "defender");
+                $("#opponentText").text("Your Opponent");
+                $("#display-fight").text("");
 
-            } else {
+            } else {    
                 return 
             } 
             console.log("push attacker: ")   
@@ -69,9 +61,16 @@ let characters =[
             if (attackCharacter.length === 0 && opponentCharacter.length === 0) {
                 attackCharacter.push(sauron)
                 $("#sauron").appendTo("#playerCharacter");
+                $("#yourCharacterText").text("Your Character");
+                $("#sa").prop("onclick", null).off("click");
+                $("#sa").attr("id", "attacker");
+                $('#characterOrOpponent').text("Choose your opponent")
             } else if (attackCharacter.length === 1 && opponentCharacter.length === 0) { 
                 opponentCharacter.push(sauron)
                 $("#sauron").appendTo("#opponent");
+                $("#sa").attr("id", "defender");
+                $("#opponentText").text("Your Opponent");
+                $("#display-fight").text("");
             } else {
                 return 
             } 
@@ -85,10 +84,16 @@ let characters =[
             if (attackCharacter.length === 0 && opponentCharacter.length === 0) {
                 attackCharacter.push(witchKing)
                 $("#witchKing").appendTo("#playerCharacter");
+                $("#yourCharacterText").text("Your Character");
+                $("#wk").prop("onclick", null).off("click");
+                $("#wk").attr("id", "attacker");
+                $('#characterOrOpponent').text("Choose your opponent")
             } else if (attackCharacter.length === 1 && opponentCharacter.length === 0) { 
                 opponentCharacter.push(witchKing)
                 $("#witchKing").appendTo("#opponent");
-
+                $("#wk").attr("id", "defender");
+                $("#opponentText").text("Your Opponent");
+                $("#display-fight").text("");
             } else {
                 
                 return 
@@ -103,9 +108,16 @@ let characters =[
             if (attackCharacter.length === 0 && opponentCharacter.length === 0) {
                 attackCharacter.push(gandalf)
                 $("#gandalf").appendTo("#playerCharacter");
+                $("#yourCharacterText").text("Your Character");
+                $("#ga").prop("onclick", null).off("click");
+                $("#ga").attr("id", "attacker");
+                $('#characterOrOpponent').text("Choose your opponent")
             } else if (attackCharacter.length === 1 && opponentCharacter.length === 0) { 
                 opponentCharacter.push(gandalf)
                 $("#gandalf").appendTo("#opponent");
+                $("#ga").attr("id", "defender");
+                $("#opponentText").text("Your Opponent");
+                $("#display-fight").text("");
             } else {
                 return 
             } 
@@ -119,14 +131,10 @@ let characters =[
 
 
 function reWriteStats() {
-    // console.log(aragorn.attack)
-    aHealthDisplay.textContent = "Health: " + aragorn.health + "  " + aragorn.attack
-    // console.log(sauron.attack)
-    sHealthDisplay.textContent = "Health: " + sauron.health + "  " + sauron.attack
-    // console.log(witchKing.attack)
-    wHealthDisplay.textContent = "Health: " + witchKing.health + "  " + witchKing.attack
-    // console.log(gandalf.attack)
-    gHealthDisplay.textContent = "Health: " + gandalf.health + "  " + gandalf.attack
+    aHealthDisplay.textContent = "Health: " + aragorn.health 
+    sHealthDisplay.textContent = "Health: " + sauron.health
+    wHealthDisplay.textContent = "Health: " + witchKing.health
+    gHealthDisplay.textContent = "Health: " + gandalf.health 
 }
 
 // GRAB ID 
@@ -136,74 +144,128 @@ let wHealthDisplay = document.getElementById('wHealth')
 let gHealthDisplay = document.getElementById('gHealth')
 
 // DISPLAY HEALTH
-aHealthDisplay.textContent = "Health: " + aragorn.health + "  " + aragorn.attack
-sHealthDisplay.textContent = "Health: " + sauron.health + "  " + sauron.attack 
-wHealthDisplay.textContent = "Health: " + witchKing.health + "  " + witchKing.attack
-gHealthDisplay.textContent = "Health: " + gandalf.health + "   " + gandalf.attack
+aHealthDisplay.textContent = "Health: " + aragorn.health 
+sHealthDisplay.textContent = "Health: " + sauron.health 
+wHealthDisplay.textContent = "Health: " + witchKing.health
+gHealthDisplay.textContent = "Health: " + gandalf.health 
 
+let newAttack = 0
 // ATTACK
 function fight() {
-
-    opponentCharacter[0].health = opponentCharacter[0].health - attackCharacter[0].attack;
-    // document.getElementById("attack").innerHTML = "Health: " + opponentCharacter[0].health;
-    reWriteStats();
-        console.log(opponentCharacter[0].health)
-    attackCharacter[0].health = attackCharacter[0].health - opponentCharacter[0].counterAttack;
-    if (opponentCharacter[0].health <= 0) {
-        emptyOpponent()
+    if (opponentCharacter.length === 0 || attackCharacter.length === 0) {
+        return
     }
-}
+    attackCharacter[0].attack = attackCharacter[0].attack + attackCharacter[0].attack 
+    //the fighting
+    opponentCharacter[0].health = opponentCharacter[0].health - attackCharacter[0].attack;
+    attackCharacter[0].health = attackCharacter[0].health - opponentCharacter[0].counterAttack;
+
+    reWriteStats();
+
+    $("#display-fight").text("You attacked " + opponentCharacter[0].name + " for " + attackCharacter[0].attack + " points");
+    $("#display-counter").text(opponentCharacter[0].name + " attacked you for " + opponentCharacter[0].counterAttack + " points")
+   
+    
+    if (opponentCharacter[0].health <= 0) {
+        $("#display-fight").text("You defeated " + opponentCharacter[0].name);
+        $("#display-counter").text("")
+        emptyOpponent()
+        changeBackground()
+        youWin()
+    }
+    if (attackCharacter[0].health <= 0) {
+        defeat();
+    } 
+}   
 
 // EMPTY OPPONANT
 function emptyOpponent () {
-    opponentCharacter = []
-    $("#opponent").remove();
+    kills.push(opponentCharacter[0]);
+        console.log("kills")
+        console.log(kills)
+    opponentCharacter = [];
+    $("#holdHere").hide();
+    $("#opponent").appendTo("#holdHere");
+    $("#opponent").removeAttr("id")
     $("#return").html('<div id="opponent"></div>')
-    increaseAttack()
+    $("#opponentText").text("");
     reWriteStats()
     return
 }
 
-function increaseAttack() {
-    attackCharacter[0].attack = attackCharacter[0].attack * 2
-}
+let kills = []
 
-
-
-
-
-
-
-
-
-
-
-
-// ================================================================================================
-// TESTING HOW TO EFFECT THE ATTACK VALUE
-
-document.onkeyup = function(event) {
-    let keyPress = event.key 
-
-    if (keyPress === 'a') {
-       aragorn.increaseAttack()
-       reWriteStats()
+function changeBackground() {
+    if (kills.length === 0) {
+        return
+    } else if (kills.length === 1) {
+        document.body.style.backgroundImage = "url('assets/images/bg4.jpg')";
+    } else if (kills.length === 2) {
+        document.body.style.backgroundImage = "url('assets/images/bg5.jpg')";
     }
-    if (keyPress === 's') {
-        sauron.increaseAttack()
-        reWriteStats()
-     }
-    if (keyPress === 'w') {
-        witchKing.increaseAttack()
-        reWriteStats()
-     }
-    if (keyPress === 'g') {
-        gandalf.increaseAttack()
-        reWriteStats()
-     }
+}
 
+function defeat() {
+    $(".attack-button").remove()
+    $("#reset-button").html('<button onclick="resetGame()" class="attack-button">Restart</button>')
+    alert("YOU HAVE BEEN DESTROYED")
+}
+
+function youWin () {
+    if (kills.length === 3) {
+        $("#display-fight").text("You Are Victorious");
+        $("#display-counter").text("");      
+        $(".attack-button").remove()
+        $("#reset-button").html('<button onclick="resetGame()" class="attack-button">Restart</button>')
+    } else {
+        return
+    }
 }
 
 
+    // function resetGame () {
+    //     attackCharacter = []
+    //     opponentCharacter = []
+    //     characters =[
+    //         aragorn = {
+    //             name: "Aragorn",
+    //             health: 120,
+    //             attack: 8,
+    //             counterAttack: 5,
+    //         },
+    //         sauron = {
+    //             name: "Sauron",
+    //             health: 180,
+    //             attack: 20,
+    //             counterAttack: 2500,
+    //         },
+    //         gandalf = {
+    //             name: "Gandalf",
+    //             health: 100,
+    //             attack: 5,
+    //             counterAttack: 5,
+    //         },
+    //         witchKing = {
+    //             name:"Witch King",
+    //             health: 150,
+    //             attack: 20,
+    //             counterAttack: 20,
+    //         },
+    //     ]
+    //     kills = []
+    //     reWriteStats ()
+    //     console.log("attacker: ")    
+    //     console.log(attackCharacter)
+    //     console.log("opponent: ")
+    //     console.log(opponentCharacter)
+    //     console.log("kills: ")
+    //     console.log(kills)
 
+    //     $("#aragorn").appendTo("#playerChoice");
+    //     $("#sauron").appendTo("#playerChoice");
+    //     $("#gandalf").appendTo("#playerChoice");
+    //     $("#witchKing").appendTo("#playerChoice");
+
+    //     $("#reset-button").hide()
+    // }
 
